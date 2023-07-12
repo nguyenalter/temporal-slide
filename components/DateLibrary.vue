83b71b24583bb98d2571c5c46d7dac2ada9006b2
Nguyen Hoang <anhnguyen3120@gmail.com>
2023-07-13 01:37:27 +0700

    add duration plugin

--- components/DateLibrary.vue
+++ components/DateLibrary.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { Temporal, toTemporalInstant } from '@js-temporal/polyfill';
 import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import advancedFormat from 'dayjs/plugin/advancedFormat';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
@@ -12,6 +13,7 @@ import * as dateFNS from 'date-fns';
 Date.prototype['toTemporalInstant'] = toTemporalInstant;
 window['Temporal'] = Temporal;
 
+dayjs.extend(duration)
 dayjs.extend(relativeTime);
 dayjs.extend(advancedFormat);
 dayjs.extend(customParseFormat);
